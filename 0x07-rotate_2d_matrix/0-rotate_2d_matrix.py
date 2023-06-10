e04529bb2e4c26ea5d6e266fe0ae936199a6a185
Degephomk <degiatb.k@gmail.com>
2023-06-09 23:31:40 -0700

    adding

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,54 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+Rotate 2D Matrix
+"""
 
 def rotate_2d_matrix(matrix):
+    """ Given an n x n 2D matrix, rotate it 90 degrees clockwise
     """
-    Rotate an n x n 2D matrix 90 degrees clockwise in-place.
+    for x, y in enumerate(zip(*reversed(matrix))):
+        matrix[x] = list(y)
 
-    Args:
-        matrix (list[list]): The 2D matrix to rotate.
+if __name__ == '__main__':
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-    Returns:
-        None. The matrix is edited in-place.
-
-    Example:
-        matrix = [[1, 2, 3],
-                  [4, 5, 6],
-                  [7, 8, 9]]
-
-        rotate_2d_matrix(matrix)
-
-        for row in matrix:
-            print(row)
-
-        Output:
-        [7, 4, 1]
-        [8, 5, 2]
-        [9, 6, 3]
-    """
-
-    n = len(matrix)
-
-    # Transpose the matrix
-    for i in range(n):
-        for j in range(i + 1, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-    # Reverse each row
-    for i in range(n):
-        left = 0
-        right = n - 1
-        while left < right:
-            matrix[i][left], matrix[i][right] = matrix[i][right], matrix[i][left]
-            left += 1
-            right -= 1
-
-# Example usage
-matrix = [[1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9]]
-
-rotate_2d_matrix(matrix)
-
-for row in matrix:
-    print(row)
-
+    """ Rotate the matrix in-place """
+    rotate_2d_matrix(matrix)
+    print(matrix)
